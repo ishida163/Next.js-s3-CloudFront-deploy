ab6ef12dc50f1816100b72daad03c44af14f1844
ishida <tmp07i@aol.com>
2024-05-18 18:41:30 +0900

    deployエラー対策(仮)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   trailingSlash: true,
 };
+
+images: { unoptimized: true }
 
 module.exports = nextConfig;
